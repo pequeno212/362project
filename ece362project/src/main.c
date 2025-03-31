7de15e3175d25db6ca4ef4b58c679ad5e8000557
Javier Aldana <javier.aldana@gmail.com>
2025-03-31 09:29:49 -0400

    dac

--- ece362project/src/main.c
+++ ece362project/src/main.c
@@ -1,22 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Weili An, Niraj Menon
-  * @date    Jan 24 2024
-  * @brief   ECE 362 Lab 4 Student template
-  ******************************************************************************
-*/
-
-/**
-******************************************************************************/
-
-// Fill out your username, otherwise your completion code will have the 
-// wrong username!
-const char* username = "aldana0";
-
-/******************************************************************************
-*/ 
-
 #include "stm32f0xx.h"
 #include <math.h>   // for M_PI
 #include <stdint.h>
@@ -38,6 +19,7 @@ extern const char font[];
 void print(const char str[]);
 // Print a floating-point value.
 void printfloat(float f);
+M_PI = 3.14159156;
 
 
 //============================================================================
@@ -78,81 +60,13 @@ void enable_dma(void) {
     DMA1_Channel5 -> CCR |= DMA_CCR_EN;
 }
 
-//============================================================================
-// init_tim15()
-//============================================================================
-void init_tim15(void) {
-    RCC -> APB2ENR |= RCC_APB2ENR_TIM15EN;
-    
-    TIM15 -> DIER = TIM_DIER_UDE;
-    TIM15 -> PSC = 240 - 1;
-    TIM15 -> ARR = 200 -1;
-    TIM15 -> CR1 |= TIM_CR1_CEN;
-}
-
-//=============================================================================
-// Part 2: Debounced keypad scanning.
-//=============================================================================
-
-uint8_t col; // the column being scanned
-
-void drive_column(int);   // energize one of the column outputs
-int  read_rows();         // read the four row inputs
-void update_history(int col, int rows); // record the buttons of the driven column
-char get_key_event(void); // wait for a button event (press or release)
-char get_keypress(void);  // wait for only a button press event.
-float getfloat(void);     // read a floating-point number from keypad
-void show_keys(void);     // demonstrate get_key_event()
-
-//============================================================================
-// The Timer 7 ISR
-//============================================================================
-// Write the Timer 7 ISR here.  Be sure to give it the right name.
-
-void TIM7_IRQHandler() {
-
-    TIM7 -> SR &= ~ TIM_SR_UIF;
-    int rows = read_rows();
-    update_history(col, rows);
-    col = (col + 1) & 3;
-    drive_column(col);
-}
-
-//============================================================================
-// init_tim7()
-//============================================================================
-void init_tim7(void) {
-    
-    RCC -> APB1ENR |= RCC_APB1ENR_TIM7EN;
-    TIM7 -> PSC = 240 -1;
-    TIM7 -> ARR = 200-1;
-    TIM7 -> DIER |= TIM_DIER_UIE;
-
-    NVIC -> ISER[0] = (1 << TIM7_IRQn);
-    TIM7 -> CR1 |= TIM_CR1_CEN;
-}
 
 //=============================================================================
 // Part 3: Analog-to-digital conversion for a volume level.
 //=============================================================================
 uint32_t volume = 2048;
 
-//============================================================================
-// setup_adc()
-//============================================================================
-void setup_adc(void) {
-    RCC -> AHBENR |= RCC_AHBENR_GPIOAEN;
 
-    GPIOA -> MODER |= 0xC;//pin a1 is ADC_IN1
-    RCC -> APB2ENR |= RCC_APB2ENR_ADC1EN;
-    RCC -> CR2 |= RCC_CR2_HSI14ON;
-    while(!(RCC -> CR2 & RCC_CR2_HSI14RDY)); //wait for 14Mhz clock to be ready
-    ADC1 -> CR |= ADC_CR_ADEN;
-    while(!(ADC1 -> ISR & ADC_ISR_ADRDY)); //wait for ADC to be ready
-    
-    ADC1 -> CHSELR |= (1 << 1);
-
-}
 //============================================================================
 // Varables for boxcar averaging.
 //============================================================================
@@ -160,40 +74,6 @@ void setup_adc(void) {
 int bcsum = 0;
 int boxcar[BCSIZE];
 int bcn = 0;
-
-//============================================================================
-// Timer 2 ISR
-//============================================================================
-// Write the Timer 2 ISR here.  Be sure to give it the right name.
-
-void TIM2_IRQHandler() {
-
-    TIM2 -> SR &= ~ TIM_SR_UIF;
-    ADC1 -> CR |= ADC_CR_ADSTART;
-    while(!(ADC1 -> ISR |= 2));
-
-    bcsum -= boxcar[bcn];
-    bcsum += boxcar[bcn] = ADC1->DR;
-    bcn += 1;
-    if (bcn >= BCSIZE)
-        bcn = 0;
-    volume = bcsum / BCSIZE;
-    
-}
-
-//============================================================================
-// init_tim2()
-//============================================================================
-void init_tim2(void) {
-    RCC -> APB1ENR |= RCC_APB1ENR_TIM2EN;
-    TIM2 -> PSC = 2400 -1;
-    TIM2 -> ARR = 2000-1;
-    TIM2 -> DIER |= TIM_DIER_UIE;
-
-    NVIC -> ISER[0] = (1 << TIM2_IRQn);
-    TIM2 -> CR1 |= TIM_CR1_CEN;
-}
-
 
 //===========================================================================
 // Part 4: Create an analog sine wave of a specified frequency
@@ -296,6 +176,10 @@ void TIM6_DAC_IRQHandler() {
 // init_tim6()
 //============================================================================
 void init_tim6(void) {
+
+    //instead of invoking DAC at freq, only do if interrupt is aknolwedged
+    //or call init_tim6 only when interrupt is enabled
+
     int div = 48000000/ RATE;
     int pscalar = div / 100;
     int arrscalar = 100;
@@ -313,9 +197,28 @@ void init_tim6(void) {
     TIM6 -> CR1 |= TIM_CR1_CEN;
 }
 
-//============================================================================
-// All the things you need to test your subroutines.
-//============================================================================
+/** 
+ * pseudocode: exti, have interrupts unmasekd when win == true, right now its set for B0, so ideally the rest of the code would set b0 to 1 to trigger audio
+ */
+void init_exti() {
+  RCC->APB2ENR |= RCC_APB2ENR_SYSCFGCOMPEN; //enable syscfg subsystem
+
+  SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PB; //selects port b0 to syscfg subsystem
+  EXTI -> RTSR |= EXTI_RTSR_TR0; //configures extir_rtsr register
+  EXTI -> IMR |= EXTI_IMR_MR0; //configures IMR register
+ 
+  
+  NVIC -> ISER[0] = (1 << EXTI0_1_IRQn); //MIGHT NEED TO SWITCH |= FOR =
+
+}
+
+void EXTI0_1_IRQHandler(){
+  
+  EXTI->PR = (EXTI_PR_PR0);
+  //EXTI->PR |= EXTI_PR_PR1;
+  init_tim6();
+}
+
 int main(void) {
     internal_clock();
     // Initialize the display to something interesting to get started.
@@ -328,46 +231,14 @@ int main(void) {
     msg[6] |= font['2'];
     msg[7] |= font[' '];
 
-    // Uncomment when you are ready to produce a confirmation code.
-    //autotest();
+   
 
     enable_ports();
     setup_dma();
     enable_dma();
-    init_tim15();
 
-    // Comment this for-loop before you demo part 1!
-    // Uncomment this loop to see if "ECE 362" is displayed on LEDs.
-    /*for (;;) {
-        asm("wfi");
-    }
-    // End of for loop
-    */
-    // Demonstrate part 1
-// #define SCROLL_DISPLAY
-#ifdef SCROLL_DISPLAY
-    for(;;)
-        for(int i=0; i<8; i++) {
-            print(&"Hello...Hello..."[i]);
-            nano_wait(250000000);
-        }
-#endif
-
-    init_tim7();
-
-    // Demonstrate part 2
-//#define SHOW_KEY_EVENTS
-#ifdef SHOW_KEY_EVENTS
-    show_keys();
-    
-#endif
-
-    setup_adc();
-    init_tim2();
-
-    // Demonstrate part 3
-//#define SHOW_VOLTAGE
-#ifdef SHOW_VOLTAGE
+#define TEST_DAC
+#ifdef TEST_DAC
     for(;;) {
         printfloat(2.95 * volume / 4096);
     }
@@ -375,27 +246,12 @@ int main(void) {
 
     init_wavetable();
     setup_dac();
+
+    //call init_tim6 when interrupt is enabled
     init_tim6();
 
-#define ONE_TONE
-#ifdef ONE_TONE
-    for(;;) {
-        float f = getfloat();
-        set_freq(0,f);
-    }
-#endif
 
-    // demonstrate part 4
-#define MIX_TONES
-#ifdef MIX_TONES
-    for(;;) {
-        char key = get_keypress();
-        if (key == 'A')
-            set_freq(0,getfloat());
-        if (key == 'B')
-            set_freq(1,getfloat());
-    }
-#endif
+
 
     // Have fun.
     dialer();
